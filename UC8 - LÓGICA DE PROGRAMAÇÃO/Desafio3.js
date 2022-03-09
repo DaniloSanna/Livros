043464cb74c3d6b76c98bb8b6b1044f9229e1b9e
DaniloSanna <danilosanna@msn.com>
2022-03-08 23:51:23 -0300

    Desafio 3 - Resolução de Datas

--- UC8 - LÓGICA DE PROGRAMAÇÃO/Desafio3.js
+++ UC8 - LÓGICA DE PROGRAMAÇÃO/Desafio3.js
@@ -1,43 +1,37 @@
 
+//console: npm install readline-sync
+//console: npm install moment
+
 var readlineSync = require('readline-sync');
+const moment = require('moment');
+let date1 = new Date(moment(new Date(), 'DD/MM/YYYY'));
+
 let participantes = [];
 
  
 //Coletando Data.
-//var data = readlineSync.question('Insira a data do Evento: ');
+var dataevento = new Date(moment(readlineSync.question('Insira a data do Evento: '), 'DD/MM/YYYY'));
 
 // Coletando nome.
-//var nome = readlineSync.question('Qual é o nome do participante: ');
+var nome = readlineSync.question('Qual é o nome do participante: ');
 
 // Coletando idade.
-//var nome = readlineSync.question('Qual é a idade do participante: ');
+var idade = readlineSync.question('Qual é a idade do participante: ');
+
+//console: npm install date-and-time e npm install dateformat
+let dtformat = new Intl.DateTimeFormat('pt-BR');
+
+console.log("Nome: " + nome + ", Data: " + dtformat.format(dataevento) + ", idade: " + idade);
 
 
-const dt = new Date().toLocaleString('pt-br', {
-    timeZone: "America/Sao_Paulo"
-  });
-
-
-  //console: npm install date-and-time e npm install dateformat
-let jao = "02/03/2022";
-
-let dates = require('date-and-time');
-
-let data = dates.format(Date.parse(jao),'YYYY/MM/DD HH:mm:ss');
-let data1 = Date.parse("04/03/2022");
-
-
-console.log ("DATA: " + data);
-console.log ("DATA1: " + data1);
-
-data.toLocaleString.toLocaleString('pt-br', {
-    timeZone: "America/Sao_Paulo"
-  });
-
-if (data > data1) {
-    console.log ("DATA > DATA1");
+/*
+console.log(date1);
+console.log(date2);
+if( date1 > date2 ){
+  console.log("Maior: " + dtformat.format(date1));
+  console.log("Menor: " + dtformat.format(date2));
 }else{
-    console.log ("DATA < DATA1");
+  console.log("Maior: " + dtformat.format(date2));
+  console.log("Menor: " + dtformat.format(date1));
 }
-
-
+*/
