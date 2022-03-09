d107e8f124b2961009a056bed997c82535a5175c
DaniloSanna <danilosanna@msn.com>
2022-03-09 01:13:24 -0300

    Array - Parte 1

--- UC8 - LÓGICA DE PROGRAMAÇÃO/Desafio3.js
+++ UC8 - LÓGICA DE PROGRAMAÇÃO/Desafio3.js
@@ -1,37 +1,66 @@
+class participante {
+  constructor(nome, idade){
+   this.nome = nome;
+   this.idade = idade;
+  }
+  get nome() {
+    return nome;
+  }
+  get idade() {
+    return idade;
+  }
+  set nome(valor) {
+    nome = valor;
+  }
+  set idade(valor) {
+    idade = valor;
+  }
+  get consoleLog(){
+    return ("Nome: " + nome + " - Idade: " + idade);
+  }
+
+  }
 
 //console: npm install readline-sync
 //console: npm install moment
-
 var readlineSync = require('readline-sync');
 const moment = require('moment');
+let dtformat = new Intl.DateTimeFormat('pt-BR');
 let date1 = new Date(moment(new Date(), 'DD/MM/YYYY'));
+var participantes = new Array([100]);
 
-let participantes = [];
-
- 
 //Coletando Data.
-var dataevento = new Date(moment(readlineSync.question('Insira a data do Evento: '), 'DD/MM/YYYY'));
+//var dataevento = new Date(moment(readlineSync.question('Insira a data do Evento: '), 'DD/MM/YYYY'));
 
 // Coletando nome.
 var nome = readlineSync.question('Qual é o nome do participante: ');
 
 // Coletando idade.
-var idade = readlineSync.question('Qual é a idade do participante: ');
+var idade = Number(readlineSync.question('Qual é a idade do participante: '));
 
-//console: npm install date-and-time e npm install dateformat
-let dtformat = new Intl.DateTimeFormat('pt-BR');
+//if( date1 > date2 ){
+  console.log("Data Válida");
+  if( idade >= 18 ){
+    console.log("Idade Válida");
+    console.log("----- ADICIONAR-----");
+    
+    var pa = new participante(nome, idade);
+    console.log("Valor de PA: " + pa.consoleLog);
+    console.log("Valor de length: " + participantes.length);
+    participantes.push(pa);
+    console.log("Valor de length: " + participantes.length);
 
-console.log("Nome: " + nome + ", Data: " + dtformat.format(dataevento) + ", idade: " + idade);
-
-
+  }else{
+    console.log("Idade Inválida");
+  }
 /*
-console.log(date1);
-console.log(date2);
-if( date1 > date2 ){
-  console.log("Maior: " + dtformat.format(date1));
-  console.log("Menor: " + dtformat.format(date2));
 }else{
-  console.log("Maior: " + dtformat.format(date2));
-  console.log("Menor: " + dtformat.format(date1));
-}
+    console.log("Data Inválida");
+  }
 */
+console.log("---------Valor Array---------");
+for (var i = 1; i<participantes.length; i++){
+    //console.log(i + " - " + participantes[i]);
+    console.log(i + " - " + participantes[i].consoleLog);
+
+}
